84f69635e02fb0a0047e4ed415901e54aaa375f5
DenverDen <denverden@mail.ru>
2020-03-16 01:10:30 +0300

    fix: project fix header, slider, portfolio

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 window.onload = function() {
     const MENU = document.querySelector('.menu');
+    const BUTTON_PORTFOLIO = document.querySelector('.portfolio-buttons');
     const BUTTON_PROJECT = document.querySelector('.portfolio-projects');
+    const PHONE_BUTTON = document.querySelectorAll(".iphone-vertical__button");
 
     MENU.addEventListener('click', (event) => {
         if(event.target.className == 'menu__link') {
@@ -9,19 +11,29 @@ window.onload = function() {
         }
     });
 
+    BUTTON_PORTFOLIO.addEventListener('click', (event) => {
+        if(event.target.className !== 'btn btn-portfolio btn-portfolio--active') { 
+            BUTTON_PORTFOLIO.querySelectorAll('.btn-portfolio').forEach(element => element.classList.remove('btn-portfolio--active'));
+            if(event.target.className !== 'portfolio-buttons') {
+                event.target.classList.add('btn-portfolio--active');
+                BUTTON_PROJECT.querySelectorAll('.btn-project').forEach(element => element.style.order = Math.floor(Math.random() * 11));    
+            }
+        }
+    });
+
     BUTTON_PROJECT.addEventListener('click', (event) => {
         BUTTON_PROJECT.querySelectorAll('.btn-project').forEach(element => element.classList.remove('btn-project--active'));
         if(event.target.className !== 'portfolio-projects') event.target.classList.add('btn-project--active');
     });
 
-    document.querySelector(".iphone-horizontal").addEventListener("click", displayPhoneHorizontal);
-    document.querySelector(".iphone-vertical").addEventListener("click", displayPhoneVertical);
+    PHONE_BUTTON.forEach(element =>
+        element.addEventListener('click', (event) => {
+            event.target.parentElement.querySelector('.iphone-vertical__screen').classList.toggle("iphone-vertical__screen--no-active");
+            event.target.parentElement.querySelector('.iphone-vertical__camera').classList.toggle("iphone-vertical__camera--no-active");
+        }));
 
-    function displayPhoneHorizontal() {
-        document.querySelector(".iphone-horizontal__screen").classList.toggle("iphone-horizontal__screen--no-active");
-    }
-    
-    function displayPhoneVertical() {
-        document.querySelector(".iphone-vertical__screen").classList.toggle("iphone-vertical__screen--no-active");
-    }
+    document.querySelector(".iphone-horizontal__button").addEventListener("click", (event) => {
+        event.target.parentElement.querySelector('.iphone-horizontal__screen').classList.toggle("iphone-horizontal__screen--no-active");
+        event.target.parentElement.querySelector('.iphone-horizontal__camera').classList.toggle("iphone-horizontal__camera--no-active");
+    });
 }
