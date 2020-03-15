eecf159c00294511b801f289bde0cfae029c28fd
DenverDen <denverden@mail.ru>
2020-03-15 13:16:19 +0300

    feat: phone screen active

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 window.onload = function() {
+    document.querySelector(".iphone-horizontal").addEventListener("click", displayPhoneHorizontal);
+    document.querySelector(".iphone-vertical").addEventListener("click", displayPhoneVertical);
 
+    function displayPhoneHorizontal() {
+        document.querySelector(".iphone-horizontal__screen").classList.toggle("iphone-horizontal__screen--no-active");
+    }
+    function displayPhoneVertical() {
+        document.querySelector(".iphone-vertical__screen").classList.toggle("iphone-vertical__screen--no-active");
+    }
 }
