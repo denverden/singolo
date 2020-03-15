9674ef8484619bd4537c7f88d6eac01ef583b970
DenverDen <denverden@mail.ru>
2020-03-15 14:52:39 +0300

    feat: header menu active

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 window.onload = function() {
+    const MENU = document.querySelector('.menu');    
+
+    MENU.addEventListener('click', (event) => {
+        MENU.querySelectorAll('.menu__link').forEach(element => element.classList.remove('menu__link--active'));
+        event.target.classList.add('menu__link--active');
+    }); 
+
     document.querySelector(".iphone-horizontal").addEventListener("click", displayPhoneHorizontal);
     document.querySelector(".iphone-vertical").addEventListener("click", displayPhoneVertical);
 
     function displayPhoneHorizontal() {
         document.querySelector(".iphone-horizontal__screen").classList.toggle("iphone-horizontal__screen--no-active");
     }
+    
     function displayPhoneVertical() {
         document.querySelector(".iphone-vertical__screen").classList.toggle("iphone-vertical__screen--no-active");
     }
